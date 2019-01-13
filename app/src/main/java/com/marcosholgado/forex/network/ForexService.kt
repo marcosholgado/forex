9d0aa03870656c0e07770042acc641e4459611ae
Marcos Holgado <marcos.holgadomolina@sky.uk>
2019-01-13 17:38:06 +0000

    Remove hardcoded api key. Should be an ENV variable in the CI

--- app/src/main/java/com/marcosholgado/forex/network/ForexService.kt
+++ app/src/main/java/com/marcosholgado/forex/network/ForexService.kt
@@ -1,6 +1,7 @@
 package com.marcosholgado.forex.network
 
 import com.google.gson.internal.LinkedTreeMap
+import com.marcosholgado.forex.BuildConfig
 import io.reactivex.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -8,11 +9,11 @@ import retrofit2.http.Query
 
 interface ForexService {
 
-    @GET("latest?access_key=55b3d7ea41ece863950d0174813e0195")
+    @GET("latest?access_key=${BuildConfig.API_KEY}")
     fun getCurrencies(@Query("base") base: String, @Query("symbols") symbols: String): Flowable<CurrenciesResponse>
 
 
-    @GET("{date}?access_key=55b3d7ea41ece863950d0174813e0195")
+    @GET("{date}?access_key=${BuildConfig.API_KEY}")
     fun compareCurrencies(@Path("date") date: String, @Query("base") base: String, @Query("symbols") symbols: String): Flowable<CurrenciesDateResponse>
 
     data class CurrenciesResponse(
